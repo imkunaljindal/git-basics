97c52d1d5ed1d6d8280dff143192740eeea6b27a
Kunal Jindal <kunaljindal@mnnit.ac.in>
2023-06-07 22:25:35 +0530

    Added thrid print statement

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
 
         System.out.println("Hello Kunal");
         System.out.println("We will all get a good job soon!!");
+
+        System.out.println("This is a change");
     }
 }
