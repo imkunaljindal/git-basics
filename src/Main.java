2debf6b44a7a292ae314265d3805cc023877a71f
Kunal Jindal <kunaljindal@mnnit.ac.in>
2023-06-07 22:22:39 +0530

    Added 2 print statements

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Kunal");
+        System.out.println("We will all get a good job soon!!");
+    }
+}
